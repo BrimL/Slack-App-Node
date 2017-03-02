5b5c67c5b79ccabffc95b183927e4627f27a94b4
BrimL <Brimeyer.L@gmail.com>
2017-03-02 11:27:11 -0500

    Smashed This Code Against Another Slack Tutorial
    Major Changes:
    - Exported Secret data to a git ignored file that will also allow for
    utilizing environment varibales in a hosted environment
    - Added tutorial for interactive slack buttons
    Minor Changes:
    - Formating changes, two spaces for an indent looks much nicer than four
    spaces.
    - Changed /ngrok to /ping for future utility functionality
    - Restructured /ping endpoint to match second tutorial's structure
    - Added success (200) return to calls to avoid slack time out messages
    in channel
    - General code clean-up to make things look nicer, moving towards ES6
    standards

--- index.js
+++ index.js
@@ -1,59 +1,130 @@
-// Import express and request modules
-var express = require('express');
-var request = require('request');
+let express = require('express')
+let request = require('request')
+let bodyParser = require('body-parser')
 
-// Store our app's ID and Secret. These we got from Step 1. 
-// For this tutorial, we'll keep your API credentials right here. But for an actual app, you'll want to  store them securely in environment variables. 
-var clientId = '123456789.123456789';
-var clientSecret = '11111a2222b3333c44444e';
+// Import our secrets if not already defined to the environment
+if(!process.env.CLIENT_ID){
+	let env = require('./env.js')
+}
+let clientId = process.env.CLIENT_ID
+let clientSecret = process.env.CLIENT_SECRET
+let verificationToken = process.env.VERIFICATION_TOKEN
 
-// Instantiates Express and assigns our app variable to it
-var app = express();
+// Define constants
+const app = express()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const PORT=4390
 
-
-// Again, we define a port we want to listen to
-const PORT=4390;
-
-// Lets start our server
+// Bring up the server
 app.listen(PORT, function () {
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Example app listening on port " + PORT);
-});
-
-
-// This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
+  console.log("Server is now running on port: " + PORT)
+})
 app.get('/', function(req, res) {
-    res.send('Ngrok is working! Path Hit: ' + req.url);
-});
+    res.send('Ngrok is working! Path Hit: ' + req.url)
+})
 
 // This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
 app.get('/oauth', function(req, res) {
-    // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
-    if (!req.query.code) {
-        res.status(500);
-        res.send({"Error": "Looks like we're not getting code."});
-        console.log("Looks like we're not getting code.");
-    } else {
-        // If it's there...
+  // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
+  if (!req.query.code) {
+    res.status(500)
+    res.send({"Error": "Looks like we're not getting code."})
+    console.log("Looks like we're not getting code.")
+  } else {
+    // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
+    request({
+      url: 'https://slack.com/api/oauth.access', //URL to hit
+      qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
+      method: 'GET', 
+      }, 
+      function (error, response, body) {
+        if (error) {
+          console.log(error)
+        } else {
+          res.json(body)
+        }
+      })
+  }
+})
 
-        // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
-        request({
-            url: 'https://slack.com/api/oauth.access', //URL to hit
-            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
-            method: 'GET', //Specify the method
+app.post('/command/ping', urlencodedParser, (req, res) =>{
+  var reqBody = req.body
+  var responseURL = reqBody.response_url
+  if (reqBody.token != verificationToken){
+    res.status(403).end('Access forbidden')
+  }else{
+  	res.status(200).end() 
+  	var message = { "text":"Slack app is currently up and available!"}
+  }
+  sendMessageToSlackResponseURL(responseURL, message)
+})
 
-        }, function (error, response, body) {
-            if (error) {
-                console.log(error);
-            } else {
-                res.json(body);
-
+app.post('/command/send-me-buttons', urlencodedParser, (req, res) =>{
+  var reqBody = req.body
+  var responseURL = reqBody.response_url
+  if (reqBody.token != verificationToken){
+    res.status(403).end('Access forbidden')
+  }else{
+  	res.status(200).end() 
+    let message = {
+      "text": "This is your first interactive message",
+      "attachments": [
+        {
+          "text": "Building buttons is easy right?",
+          "fallback": "Shame... buttons aren't supported in this land",
+          "callback_id": "button_tutorial",
+          "color": "#3AA3E3",
+          "attachment_type": "default",
+          "actions": [
+            {
+              "name": "yes",
+              "text": "yes",
+              "type": "button",
+              "value": "yes"
+            },
+            {
+              "name": "no",
+              "text": "no",
+              "type": "button",
+              "value": "no"
+            },
+            {
+              "name": "maybe",
+              "text": "maybe",
+              "type": "button",
+              "value": "maybe",
+              "style": "danger"
             }
-        })
+          ]
+        }
+      ]
     }
+    sendMessageToSlackResponseURL(responseURL, message)
+  }
+})
 
-    // Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
-    app.post('/command', function(req, res) {
-        res.send('Your ngrok tunnel is up and running!');
-    });
-});
+app.post('/actions', urlencodedParser, (req, res) =>{
+  res.status(200).end() 
+  let actionJSONPayload = JSON.parse(req.body.payload) 
+  let message = {
+    "text": actionJSONPayload.user.name+" clicked: "+actionJSONPayload.actions[0].name,
+    "replace_original": false
+  }
+  sendMessageToSlackResponseURL(actionJSONPayload.response_url, message)
+})
+
+function sendMessageToSlackResponseURL(responseURL, JSONmessage){
+  var postOptions = {
+    uri: responseURL,
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    json: JSONmessage
+  }
+  request(postOptions, (error, response, body) => {
+    if (error){
+      // handle errors as you see fit
+    }
+  })
+}
